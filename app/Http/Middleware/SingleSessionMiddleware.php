527a66c75a51a0d8c6788192336d0bb26f90ccbe
= <=>
2024-05-28 20:35:20 +0700

    fixx aaaaa bbb

---
+++ app/Http/Middleware/SingleSessionMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class SingleSessionMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+        if ($user && $user->session_token !== session('session_token')) {
+            return redirect()->route('login')->with('error', 'You have been logged out from other device.');
+        }
+
+        return $next($request);
+    }
+}
--- routes/auth.php
+++ routes/auth.php
@@ -17,8 +17,10 @@ Route::middleware('guest')->group(function () {
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+    Route::middleware('singleSession')->group(function () {
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])
+            ->name('login');
+    });
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
