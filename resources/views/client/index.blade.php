97652c25e91c4b4c374dd1a3ce67d274decce1bc
= <=>
2024-05-29 21:02:46 +0700

    fix view all

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -17,7 +17,7 @@
                             </div>
                             <div class="col-lg-4 col-md-4 col-sm-4">
                                 <div class="btn__all">
-                                    <a href="{{route('client.theloai', 'chieu-rap-nuoc-ngoai')}}" class="primary-btn">View All <span class="arrow_right"></span></a>
+                                    <a href="{{route('client.theloai', 'chieu-rap-nuoc-ngoai')}}" class="primary-btn">Xem tất cả<span class="arrow_right"></span></a>
                                 </div>
                             </div>
                         </div>
@@ -53,7 +53,7 @@
                             </div>
                             <div class="col-lg-4 col-md-4 col-sm-4">
                                 <div class="btn__all">
-                                    <a href="{{route('client.theloai', 'chieu-rap-viet')}}" class="primary-btn">View All <span class="arrow_right"></span></a>
+                                    <a href="{{route('client.theloai', 'chieu-rap-viet')}}" class="primary-btn">Xem tất cả<span class="arrow_right"></span></a>
                                 </div>
                             </div>
                         </div>
@@ -92,7 +92,7 @@
                             </div>
                             <div class="col-lg-4 col-md-4 col-sm-4">
                                 <div class="btn__all">
-                                    <a href="{{route('client.theloai', 'phim-hay-khac')}}" class="primary-btn">View All <span class="arrow_right"></span></a>
+                                    <a href="{{route('client.theloai', 'phim-hay-khac')}}" class="primary-btn">Xem tất cả<span class="arrow_right"></span></a>
                                 </div>
                             </div>
                         </div>
